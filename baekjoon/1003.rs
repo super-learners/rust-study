cadbe000eab1a25563f926e345903a8f8db4c44a
j30ng <leej0116@outlook.com>
2019-01-11 18:59:38 +0900

    Add 1003.rs; doesn't compile

---
+++ baekjoon/1003.rs
@@ -0,0 +1,49 @@
+use std::io;
+use std::collections::HashMap;
+
+fn main() {
+    let num_of_testcases = read_line_from_stdin().trim()
+        .parse().expect("Error parsing the input");
+
+    for _i in 0..num_of_testcases {
+        let num = read_line_from_stdin().trim()
+            .parse().expect("Error parsing the input");
+        let answer = solve_inefficiently(num);
+        println!("{} {}", answer.0, answer.1);
+    }
+}
+
+fn read_line_from_stdin() -> String {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Error reading from stdin");
+        return input;
+}
+
+fn solve_inefficiently(num: i32) -> (i32, i32) {
+    fn simulate_fibonacci(num: i32, tup: &mut (i32, i32)) -> (i32, i32) {
+        if num == 0 {
+            tup.0 += 1;
+        } else if num == 1 {
+            tup.1 += 1;
+        } else {
+            simulate_fibonacci(num - 1, tup);
+            simulate_fibonacci(num - 2, tup);
+        }
+        *tup
+    }
+    simulate_fibonacci(num, &mut (0, 0))
+}
+
+fn solve_with_cheatsheet(num: i32) -> (i32, i32) {
+    let mut cheatsheet: HashMap<i32, (i32, i32)> = HashMap::new();
+    cheatsheet.insert(0, (1, 0));
+    cheatsheet.insert(1, (0, 1));
+
+    for i in 2..(num + 1) {
+        let pp = cheatsheet.get(&(i - 2)).expect("impossible");
+        let p  = cheatsheet.get(&(i - 1)).expect("impossible");
+        cheatsheet.insert(i, (pp.0 + p.0, pp.1 + p.1));
+    }
+
+    *cheatsheet.get(&num).expect("impossible")
+}
