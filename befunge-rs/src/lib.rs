867d65497f8393c7600829ad14677d6ec16b7fac
Byoung Gi Lee <handrake@gmail.com>
2019-05-02 20:57:38 +0900

    quine 테스트 추가

--- befunge-rs/src/lib.rs
+++ befunge-rs/src/lib.rs
@@ -13,4 +13,13 @@ mod tests {
 
         assert_eq!(output, "Hello, World!\n");
     }
+
+    #[test]
+    fn test_quine() {
+        let cmd = String::from("01->1# +# :# 0# g# ,# :# 5# 8# *# 4# +# -# _@");
+        let mut interpreter: Environment = Interpreter::new();
+        let output = interpreter.run(&cmd);
+
+        assert_eq!(output, "01->1# +# :# 0# g# ,# :# 5# 8# *# 4# +# -# _@");
+    }
 }
